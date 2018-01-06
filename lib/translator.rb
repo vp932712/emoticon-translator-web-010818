1a41720db93fbefdfb1e257b9cc24d1309a5b47f
Vic Patel <vp932712@gmail.com>
2018-01-06 19:24:40 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,18 +17,18 @@ end
 
 def get_japanese_emoticon(path, emoticon)
   emoticon_hash = load_library(path)
-  result = emoticon_hash["get_emoticon"][emoticon]
-  if result == nil
-    result = "Sorry, that emoticon was not found"
+  final = emoticon_hash["get_emoticon"][emoticon]
+  if final == nil
+    final = "Sorry, that emoticon was not found"
   end
-  result
+  final
 end
 
 def get_english_meaning(path, emoticon)
   emoticon_hash = load_library(path)
-  result = emoticon_hash["get_meaning"][emoticon]
-  if result == nil
-    result = "Sorry, that emoticon was not found"
+  final = emoticon_hash["get_meaning"][emoticon]
+  if final == nil
+    final = "Sorry, that emoticon was not found"
   end
-  result
+  final
 end
